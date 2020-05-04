408841652d439a4258c99299ada997f65b5a22e1
EndlessSlow <nanohana2016s@gmail.com>
2020-05-04 11:02:24 +0900

    first commit

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>a single user, width ID : {{ this.$route.params.id }}</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/users/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>
+    <input type="text" v-model="userId" />
+    <button @click="onLoadUser">Load User</button>
+    The Users Component View
+  </h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: ""
+    };
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push(`/users/${this.userId}`);
+    }
+  }
+};
+</script>
+
+<style></style>
