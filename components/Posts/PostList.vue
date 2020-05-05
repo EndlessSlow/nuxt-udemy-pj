5eb0fd8e68f866c8ba6145549a7cb88b73d1f7b2
EndlessSlow <nanohana2016s@gmail.com>
2020-05-05 14:28:48 +0900

    section 3 - 34 creating an admin section.

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="posts-page">
+    <section class="post-list">
+      <PostPreview
+        id="1"
+        thumbnail="https://cdn.wallpapersafari.com/97/84/FojnuC.jpg"
+        title="Heelo there!"
+        previewText="this my first posts"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://cdn.wallpapersafari.com/97/84/FojnuC.jpg"
+        title="Heelo there!2"
+        previewText="this my first posts"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://cdn.wallpapersafari.com/97/84/FojnuC.jpg"
+        title="Heelo there!3"
+        previewText="this my first posts"
+      />
+    </section>
+  </div>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview.vue'
+export default {
+  name: 'PostList',
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-self: center;
+  align-items: center;
+}
+
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <PostList />
+</template>
+
+<script>
+import PostList from '@/components/Posts/PostList.vue'
+export default {
+  components: {
+    PostList
+  }
+}
+</script>
+
